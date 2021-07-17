49009261578fde4f9f2a1206e7521a71d7653ce8
Stephane Gagnon <stephane-gagnon@hotmail.com>
2021-07-16 21:56:05 -0400

    Average test

--- dth11.py
+++ dth11.py
@@ -18,17 +18,25 @@ def main(args=None):
         # GPIO17, GPIO27
         # Pin 11, Pin 13
         pins = [17, 27]
+        divider = len(pins)
         while True:
+            h = 0
+            t = 0
             for pin in pins:
                 humidity, temperature = Adafruit_DHT.read_retry(11, pin)
-                if humidity is not None and temperature is not None:
+                if humidity and temperature:
+                    t += temperature
+                    h += humidity
                     print(format(temperature, '.1f'))
                     print(format(humidity, '.1f'))
                     #log.info('{0:0,.1f};{1:0,.1f}'.format(temperature, humidity).replace('.',','))
                 else:
                     print('Read error on pin number ' + pin)
                     #log.info('{0:0.1f};{1:0.1f}'.format(0, 0).replace('.',','))
-            time.sleep(10)
+            if t and h:
+                print(format(t/divider, '.1f'))
+                print(format(h/divider, '.1f'))
+            time.sleep(5)
     except (KeyboardInterrupt):
         print('T:{0:0.1f};H:{1:0.1f}'.format(0, 0).replace('.',','))
         #log.info('{0:0.1f};{1:0.1f}'.format(0, 0).replace('.',','))
